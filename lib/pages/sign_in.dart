10056357253d72a4064a7e647ef2a7fd2c110bee
eddieware <47250727+eddieware@users.noreply.github.com>
2020-08-16 12:30:25 -0500

    get_emmail_and_pass

---
+++ lib/pages/sign_in.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Signin_Page extends StatefulWidget {
+  Signin_Page({Key key}) : super(key: key);
+
+  @override
+  _Signin_PageState createState() => _Signin_PageState();
+}
+
+class _Signin_PageState extends State<Signin_Page> {
+  TextEditingController _txtPassCntrllr = new TextEditingController();
+  TextEditingController _txtEmailCntrllr = new TextEditingController();
+
+  String _email = '';
+  String _pass = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text('Food App'),
+          backgroundColor: CupertinoColors.activeOrange,
+        ),
+        child: ListView(
+          children: <Widget>[
+            _crearEmail(),
+            _crearPassword(),
+            _crearBoton(),
+          ],
+        ));
+  }
+
+  Widget _crearEmail() {
+    return CupertinoTextField(
+      controller: _txtEmailCntrllr,
+      prefix: Icon(
+        CupertinoIcons.mail_solid,
+        color: CupertinoColors.extraLightBackgroundGray,
+        size: 35.0,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 12.0),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      keyboardType: TextInputType.emailAddress,
+      autocorrect: false,
+      decoration: BoxDecoration(
+        border: Border(
+            bottom:
+                BorderSide(width: 0.5, color: CupertinoColors.inactiveGray)),
+      ),
+      placeholder: 'Email',
+      onChanged: (value) {
+        value = _txtEmailCntrllr.toString();
+        Text(value);
+      },
+    );
+  }
+
+  Widget _crearPassword() {
+    return CupertinoTextField(
+      controller: _txtPassCntrllr,
+      prefix: Icon(
+        CupertinoIcons.padlock_solid,
+        color: CupertinoColors.extraLightBackgroundGray,
+        size: 35.0,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 12.0),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      keyboardType: TextInputType.visiblePassword,
+      autocorrect: false,
+      decoration: BoxDecoration(
+        border: Border(
+            bottom:
+                BorderSide(width: 0.5, color: CupertinoColors.inactiveGray)),
+      ),
+      placeholder: 'Type a password',
+    );
+  }
+
+  Widget _crearBoton() {
+    return CupertinoButton.filled(
+      child: Text('Sign In'),
+      onPressed: _signFunction,
+      padding: EdgeInsets.symmetric(horizontal: 20),
+    );
+  }
+
+  void _signFunction() {
+    //print('SIGNIN FUNCTION');
+    print('${_txtEmailCntrllr.text}');
+    print('${_txtPassCntrllr.text}');
+  }
+}
